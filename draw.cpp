4f6ce3d5ef30ac370362aa55732bba22c012c37a
岩下稔 THS24 <ths41188@ths.hal.ac.jp>
2025-03-05 11:42:49 +0900

    スクロールができるようになったが、画面表示がバグるので修正が必要

--- draw.cpp
+++ draw.cpp
@@ -1,5 +1,6 @@
 #include "draw.h"
 
+int ScrollY = 0; // スクロール量
 
 // 描画処理
 void DrawField()
@@ -10,6 +11,7 @@ void DrawField()
             {
                 float posx = field[i].PositionX;
                 float posy = field[i].PositionY;
+                
 
                 for (int y = field[i].Height - 1; y >= 0; y--)
                 {
@@ -21,14 +23,14 @@ void DrawField()
 
                     for (int x = 0; x < field[i].Width; x++)
                     {
-                        gotoxy((int)(posx + x), (int)(posy + y));
+                        gotoxy((int)(posx + x), (int)(posy + y + ScrollY));
                         std::cout << "=";
                     }
                 }
             }
         }
-}
 
+}
 
 void DrawPlayer() // 描画
 {
@@ -37,17 +39,37 @@ void DrawPlayer() // 描画
 
  
         // 前回のプレイヤーを消去
-        gotoxy(Player->PositionXOld, Player->PositionYOld);
+        gotoxy(Player->PositionXOld, Player->PositionYOld+ScrollY);
         std::cout << " "; // 設定した座標にスペースを表示して消去する
 
         // 文字の表示色を変更
         textcolor(RED); // 文字を赤に変更
 
         // 現在のプレイヤーを表示
-        gotoxy(Player->PositionX, Player->PositionY);
+        gotoxy(Player->PositionX, Player->PositionY+ScrollY);
         std::cout << "P"; // とりあえずP
    
         Player->PositionXOld = Player->PositionX; // 現在の座標を保存
         Player->PositionYOld = Player->PositionY;
     
 }
+
+void DrawScroll()
+{
+    //プレイヤーが三段登ったら呼び出す
+
+    if (PlayerUpCount != 0 && PlayerUpCount % 3 == 0)
+    {
+		do
+        {
+            clrscr();
+			std::cout << "CameraMove\n";//デバッグ用
+			ScrollY += 6;
+			PlayerUpCount = 0;
+		} while (PlayerUpCount!=0);
+    }
+}
+
+//clrscr();
+//std::cout << "CameraMove\n";
+//ScrollY += 6;
